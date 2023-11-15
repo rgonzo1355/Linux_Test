d1386d24e55a6d1dde80bf8825f4be4d52c9b36c
Rodolfo Gonzalez <144846763+rgonzo1355@users.noreply.github.com>
2023-11-15 08:37:39 -0800

    Update TeamViewer4.ps1
    5

--- TeamViewer4.ps1
+++ TeamViewer4.ps1
@@ -1,32 +1,28 @@
 # Check if TeamViewer is installed in the standard directory
 if (Test-Path "C:\Program Files (x86)\TeamViewer\TeamViewer.exe") {
-    Write-Host "TeamViewer is installed in the standard directory. Opening..."
-    Start-Process "C:\Program Files (x86)\TeamViewer\TeamViewer.exe"
-    exit  # Exit the script after opening TeamViewer
+  Write-Host "TeamViewer is installed in the standard directory. Opening..."
+  Start-Process "C:\Program Files (x86)\TeamViewer\TeamViewer.exe"
+  exit # Exit the script after opening TeamViewer
 }
 # Check if TeamViewer is installed in the "Program Files" directory
 elseif (Test-Path "C:\Program Files\TeamViewer\TeamViewer.exe") {
-    Write-Host "TeamViewer is installed in the 'Program Files' directory. Opening..."
-    Start-Process "C:\Program Files\TeamViewer\TeamViewer.exe"
-    exit  # Exit the script after opening TeamViewer
+  Write-Host "TeamViewer is installed in the 'Program Files' directory. Opening..."
+  Start-Process "C:\Program Files\TeamViewer\TeamViewer.exe"
+  exit # Exit the script after opening TeamViewer
 }
 else {
-    # TeamViewer is not installed, so install it
-    Write-Host "TeamViewer is not installed. Installing..."
+  # TeamViewer is not installed, so install it
+  Write-Host "TeamViewer is not installed. Installing..."
 
-    # Download TeamViewer installer
-    $teamViewerInstallerUrl = "https://download.teamviewer.com/download/TeamViewer_Setup.exe"
-    $teamViewerInstallerPath = "$env:TEMP\TeamViewer_Setup.exe"
-    Invoke-WebRequest -Uri $teamViewerInstallerUrl -OutFile $teamViewerInstallerPath
+  # Download TeamViewer installer
+  $teamViewerInstallerUrl = "https://download.teamviewer.com/download/TeamViewer_Setup.exe"
+  $teamViewerInstallerPath = "$env:TEMP\TeamViewer_Setup.exe"
+  Invoke-WebRequest -Uri $teamViewerInstallerUrl -OutFile $teamViewerInstallerPath
 
-    # Install TeamViewer silently
-    Start-Process -FilePath $teamViewerInstallerPath -ArgumentList "/S" -Wait
+  # Install TeamViewer silently
+  Start-Process -FilePath $teamViewerInstallerPath -ArgumentList "/S" -Wait
 
-    Write-Host "TeamViewer installed successfully."
-    
-    # Open TeamViewer
-    Start-Process "C:\Program Files (x86)\TeamViewer\TeamViewer.exe"
-    exit  # Exit the script after opening TeamViewer
+  Write-Host "TeamViewer installed successfully."
 }
 
 # The script should not reach this point
